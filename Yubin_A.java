b6846947f4e164f035459f9fde20febb689e9bfb
hideaki-marker <83158804+hideaki-marker@users.noreply.github.com>
2021-09-06 09:33:22 +0900

    Add files via upload

---
+++ Yubin_A.java
@@ -0,0 +1,79 @@
+package chap05;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Yubin_A {
+	// フィールド
+	private String postcode;
+	private String todo;
+	private String shi;
+	private String cho;
+	// コンストラクター
+	public Yubin_A(){}
+	//第二のコンストラクター
+	public Yubin_A(String postcode,String todo,String shi,String cho){
+		this.postcode 	= postcode;
+		this.todo 		= todo;
+		this.shi 		= shi;
+		this.cho 		= cho;
+	}
+	// アクセスメソッド
+	public String getPostcode() {
+		return postcode;
+	}
+	public String getTodo() {
+		return todo;
+	}
+	public String getShi() {
+		return shi;
+	}
+	public String getCho() {
+		return cho;
+	}
+	/*	引数(郵便番号)でデータベース郵便テーブル(yubin)
+		より、郵便情報（都道府県、市区町村、町域を持つ
+		Yubinクラスのオブジェクト）を返す。
+	*/
+	public static Yubin_A getYubin(String postcode){//オブジェクトを返す
+		Yubin_A 				yub = null;
+		Connection 			con = null;
+		PreparedStatement 	pst = null;
+		ResultSet 			rs 	= null;
+		String 				sql = "SELECT * FROM yubin WHERE postcode = ?";
+		try{
+			/*コネクションインターフェース型に接続を実装*/
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/test", "root","root");
+			//SQL文の送信、結果のやり取りを行うオブジェクトの生成
+			pst = con.prepareStatement(sql);
+			pst.setString(1, postcode); //第二引数は代入する値
+			//SQL文を実行してDBから結果セットを得る
+			rs = pst.executeQuery();
+			while(rs.next()){ //結果セットの行数がある限り繰り返す
+				//第二のコンストラクターでオブジェクトの作成
+				yub = new  Yubin_A(rs.getString("postcode"),
+						         rs.getString("todo"),
+						         rs.getString("shi"),
+						         rs.getString("cho"));
+			}
+		}catch(SQLException e){
+			System.out.println("郵便番号検索中に障害が発生しました！");
+			System.out.println(pst);
+			e.printStackTrace();
+		}finally{
+			try{
+				//クローズ処理
+				if(rs != null) rs.close();
+				if(pst != null) pst.close();
+				if(con != null) con.close();
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+		}
+		return yub;//オブジェクトを返す
+	}
+}
--- ZipSearch_A.java
+++ ZipSearch_A.java
@@ -1,63 +1,31 @@
-package chap07;
+package chap05;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * コンソールから入力した郵便番号に対応する住所を表示する。
- */
 public class ZipSearch_A {
-
 	public static void main(String[] args) {
+		String postcode ="";
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+			while(!(postcode.equals("q"))){
+				System.out.print("郵便番号を入力して下さい：");
+				postcode = br.readLine();
+				if(!(postcode.equals("q"))){
+					Yubin_A yub = Yubin_A.getYubin(postcode);
+					if(yub == null){
+						System.out.println("郵便番号:" + postcode + "に該当する住所はありません");
+					}else{
+						System.out.println("郵便番号:"+yub.getPostcode()+ " " + yub.getTodo()+ " " + yub.getShi() + " " + yub.getCho());
 
-		try (BufferedReader br = new BufferedReader
-				(new InputStreamReader(System.in));) {
-			String yuubin;
-			do {
-				System.out.print("郵便番号を入力してください（qで終了） ： ");
-				yuubin = br.readLine();
-				if (!yuubin.equals("q")) {
-					String address = getAddress(yuubin);
-					System.out.println("〒" + yuubin + " : " + address);
-				}
-			} while (!yuubin.equals("q"));
-		} catch (FileNotFoundException e) {
-			System.out.println("郵便番号ファイルが存在しません。");
-		} catch (IOException e) {
-			System.out.println("入力エラーが発生しました。");
-		} catch (Exception e) {
-			System.out.println("エラーが発生しました。");
-		}
-		System.out.println("終了します。");
-	}
-	/*
-	 * 引数の郵便番号に該当する住所情報を郵便番号ファイルより取得し戻す。
-	 */
-	public static String getAddress(String yuubin) throws IOException {
-		String address = "該当する住所はありません。";
-		// 郵便番号ファイルのオープン
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(
-				new FileInputStream("01HOKKAI.CSV"), "MS932"));) {
-			String rec = "";
-			// ファイルのデータを読み込む
-			while ((rec = br.readLine()) != null) {
-				rec = rec.replace("\"", "");
-				String[] recArr = rec.split(","); // １行を要素単位に分割
-				String y_no = recArr[2];
-				if (yuubin.equals(y_no)) {
-					// ダブルクォーテーションを除いた値を格納
-					String todofuken = recArr[6];
-					String shikuchoson = recArr[7];
-					String choiki = recArr[8];
-					// 住所情報の編集
-					address = todofuken + shikuchoson + choiki;
-					break;
+					}
 				}
 			}
-			return address;
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			System.out.println("終了します。");
 		}
 	}
+
 }
